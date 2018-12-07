2bcfae59e8d81c0b3ed577b98ecbc4ac08a8a429
Polina <powertyuio1312@gmail.com>
2018-12-07 21:19:29 +0300

    new

--- www/js/loading.js
+++ www/js/loading.js
@@ -1,7 +1,7 @@
 $(window).on('load', function () {
     var $preloader = $('#page-preloader'),
     $spinner = $preloader.find('.spinner');
-    setTimeout(function(){$('#page-preloader').fadeOut('slow')},50); 
+    setTimeout(function(){$('#page-preloader').fadeOut('slow')},500); 
 });
 
     /* 
